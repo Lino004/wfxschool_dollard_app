8bc06eadfbfb0ce2b1ce8e02c48168e7de3effda
HOUNKANRIN AYINLA Mouhayad I <40716257+Lino004@users.noreply.github.com>
2021-03-17 08:32:03 +0000

    correction des index api

--- src/api/payement/index.js
+++ src/api/payement/index.js
@@ -1,22 +1,22 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const CONFIG = {
   baseURL: 'https://backend.wfxschool.com/payment/api',
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json'
     // 'Access-Control-Allow-Origin': '*',
-  },
-};
-const HTTP = axios.create(CONFIG);
+  }
+}
+const HTTP = axios.create(CONFIG)
 
-export async function initPayement(id, info) {
-  const response = await HTTP.put(`fedapay/transaction/${id}`, info);
-  return response;
+export async function initPayement (id, info) {
+  const response = await HTTP.put(`fedapay/transaction/${id}`, info)
+  return response
 }
 
-export async function createCharge(id, info) {
-  const response = await HTTP.put(`coinbase/charg/${id}`, info);
-  return response;
+export async function createCharge (id, info) {
+  const response = await HTTP.put(`coinbase/charg/${id}`, info)
+  return response
 }
 
-export default HTTP;
+export default HTTP
--- src/api/transactions/index.js
+++ src/api/transactions/index.js
@@ -1,11 +1,11 @@
-import HTTP from '../HTTP';
+import HTTP from '../HTTP'
 
-export async function createTransaction(info) {
-  const response = await HTTP.put('tasks/transaction/create', info);
-  return response;
+export async function createTransaction (info) {
+  const response = await HTTP.put('tasks/transaction/create', info)
+  return response
 }
 
-export async function updateStatusTransaction(id, status) {
-  const response = await HTTP.put(`tasks/transaction/update/satus/${id}/${status}`);
-  return response;
+export async function updateStatusTransaction (id, status) {
+  const response = await HTTP.put(`tasks/transaction/update/satus/${id}/${status}`)
+  return response
 }
